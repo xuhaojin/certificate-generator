75c4412f87e98128908a65436f55e9919d97d1fe
徐浩进 <xuhaojin@travelsky.com>
2020-03-30 23:56:14 +0800

    完成生成证照总流程方法的前一部分

--- certificate-generator/src/main/java/com/x/certificate/CertificateGenerator.java
+++ certificate-generator/src/main/java/com/x/certificate/CertificateGenerator.java
@@ -1,12 +1,20 @@
 package com.x.certificate;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.xmlbeans.XmlException;
+import org.springframework.core.io.ClassPathResource;
 
+import com.x.certificate.doc.DocOperator;
 import com.x.certificate.doc.domain.CertificateData;
 import com.x.certificate.doc.domain.CertificateField;
+import com.x.certificate.path.TempPathGenerator;
+import com.x.certificate.path.domain.TempFilePath;
+import com.x.certificate.properties.PropertiesReader;
 
 /** 
  * 证件图片生成器
@@ -15,21 +23,29 @@ import com.x.certificate.doc.domain.CertificateField;
  */
 public class CertificateGenerator {
 
-	public static void generate(CertificateData data) throws IOException {
-		// TODO 获取生成证书预设参数
-		
-		// TODO 获取证书模板
-		
-		// TODO 生成临时文件路径
-		
-		// TODO 复制证书模板，将字段替换为自定义数据
-		
+	public static void generate(CertificateData data) throws IOException, XmlException {
+		// 获取生成证书预设参数
+		Properties properties = PropertiesReader.getProperties("config\\certificate.properties");
+		String officePath = properties.getProperty("libreoffice.path");
+		String imageSuffix = properties.getProperty("certificate.image.suffix");
+		String imageScale = properties.getProperty("certificate.image.scale");
+
+		// 获取证书模板
+		File docxTemplate = new ClassPathResource("template\\template.docx").getFile();
+
+		// 生成临时文件路径
+		List<TempFilePath> tempPaths = TempPathGenerator.generateTempPaths("docx", "pdf", imageSuffix);
+
+		// 复制证书模板，将字段替换为自定义数据
+		String tempDocxPath = tempPaths.get(0).getPathName();
+		DocOperator.toCumstomDoc(docxTemplate, tempDocxPath, data);
+
 		// TODO 将doc模板转成pdf
-		
+
 		// TODO 将pdf转成证书图片
-		
+
 		// TODO 删除生成证书使用的临时文件
-		
+
 	}
 
 	public static void generate(CertificateData data, String imageSuffix) {
@@ -61,7 +77,7 @@ public class CertificateGenerator {
 
 		return sb.toString();
 	}
-	
+
 	public static void main(String[] args) throws IOException, XmlException {
 		// String templatePath = "C:\\Users\\a1579\\Desktop\\template.docx";
 		// String outputPath = "C:\\Users\\a1579\\Desktop\\custom.docx";
